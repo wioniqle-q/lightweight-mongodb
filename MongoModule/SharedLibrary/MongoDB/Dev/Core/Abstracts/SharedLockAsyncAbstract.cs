e882cea4acf8df718201e91458226f94c655f180
Tuna KARA <69215407+wioniqle-q@users.noreply.github.com>
2024-09-19 21:09:27 +0300

    .

--- MongoModule/SharedLibrary/MongoDB/Dev/Core/Abstracts/SharedLockAsyncAbstract.cs
+++
@@ -1,8 +0,0 @@
-﻿using MongoDB.Dev.Core.Interfaces;
-
-namespace MongoDB.Dev.Core.Abstracts;
-
-public abstract class SharedLockAsyncAbstract : ISharedLockAsync
-{
-    public abstract IAsyncSemaphore AsyncSemaphore { get; }
-}
